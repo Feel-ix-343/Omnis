6186254980ea293a9ad21dc8a5e853770b7c120b
Vigneshpeddi <36428047+Vigneshpeddi@users.noreply.github.com>
2023-02-06 21:47:10 -0500

    Update main.py
    Added:
    User can choose which difficulty they like first
    User can choose to add the pomodoro technique
    User can choose a maximum time for each taask
    User can choose a maximum work time for the entire day
    User can choose when to begin their work day

--- main.py
+++ main.py
@@ -16,14 +16,41 @@ def isAvaliableTime(hours1, minutes1, hours2, minutes2, duration1, duration2):
 tasks = [[]]
 moreTasks = True
 while moreTasks:
-	time = int(input("How long do you expect this task to take you (in minutes)? ")) + 10 # include article on how to estimate time on UI, 10 is for breaks
+	time = int(input("How long do you expect this task to take you (in minutes)? ")) # include article on how to estimate time on UI, 10 is for breaks
 	priority = int(input("On a scale of 1-10, how importance is this task for you? ")) # include article on how to estimate difficulty on UI
 	deadline = int(input("How many minutes away from January 1st is this task due? "))
-	tasks.append(time, deadline, priority)
+	difficulty = input("What is the difficulty [easy/medium/hard]? ")
+	ordering = input("Do you like easy or hard tasks first? ")
+	pomodoro = input("Would you like to use the pomodoro technique[y/n]? ")
+	maxTime = int(input("How long can you work on a task in minutes? "))
+	durationTime = int(input("How long do you want to work in a day in minutes? "))
+	startHour = int(input("What hour of the day would you like to begin work? "))
+	startMinutes = int(input("What minutes of that hour would you like to begin work? "))
+	if ordering == "easy":
+		if difficulty == "easy":
+			preference = 1
+		if difficulty == "medium":
+			preference = 2
+		if difficulty == "hard":
+			preference = 3
+	if ordering == "hard":
+		if difficulty == "easy":
+			preference = 3
+		if difficulty == "medium":
+			preference = 2
+		if difficulty == "hard":
+			preference = 1
+	if time < maxTime:
+		tasks.append(time,deadline,priority, preference)
+	else:
+		while time > maxTime:
+			tasks.append(maxTime, deadline, priority, preference)
+			time -= maxTime
+		tasks.append(time, deadline, priority, preference)
 	moreTasksInput = input("Do you have any more tasks [y/n]? ")
 	moreTasks = True if moreTasksInput = 'y' else 'no'
 
-tasks = sorted(tasks, key = lambda x : (x[1], x[2]))
+tasks = sorted(tasks, key = lambda x : (x[1], x[2], x[3]))
 
 obstacles = [[]]
 moreObstacles = True
@@ -40,22 +67,26 @@ daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturd
 hour = 0
 minutes = 0
 for day in range(365):
+	dayTime = 0
 	dayOfTheWeek = daysOfTheWeek[day % 7]
 	for task in tasks:
-		hour = 8
-		minutes = 0
+		hour = startHour
+		minutes = startMinutes
 		avaliableTime = False
 		while hour < 24 and !avaliableTime:
+			if pomodoro == "y":
+				task[0] += 40
 			avaliableTime = True
 			for obstacle in obstacles:
 				if dayOfTheWeek in obstacle[0]:
-					avaliableTime = isAvaliableTime(hour, minutes, obstacle[1], obstacle[2], task[1], obstacle[3])
+					avaliableTime = isAvaliableTime(hour, minutes, obstacle[1], obstacle[2], task[0], obstacle[3])
 			for item in schedule:
-				avalaibleTime = isAvaliableTime(hour, minutes, item[0], item[1], task[1], item[2])
-			minutes += task[1]
+				avalaibleTime = isAvaliableTime(hour, minutes, item[0], item[1], task[0], item[2])
+			minutes += task[0]
 			if minutes > 60:
 				hour += 1
-		if hour >= 24:
+		if hour >= 24 or dayTime + task[0] > durationTime:
 			continue
 		else:
-			schedule[0].append(hour, minutes, task[1])
+			schedule[0].append(hour, minutes, task[0])
+			dayTime += task[0]
