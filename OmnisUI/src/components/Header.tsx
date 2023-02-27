089cace8e0ac014902af1bc5df6596718b945862
Felix Zeller <felixazeller@gmail.com>
2023-02-26 21:10:28 -0500

    Update header z index

--- OmnisUI/src/components/Header.tsx
+++ OmnisUI/src/components/Header.tsx
@@ -3,6 +3,6 @@ import { JSXElement } from "solid-js";
 export default function Header(props: {children: JSXElement, class?: string}) {
   // TODO: implement this
   return (
-    <div class={ "fixed bg-background-secondary rounded-br-3xl rounded-bl-3xl shadow-lg p-10 max-h-40 top-0 right-0 left-0" + props.class }>{props.children}</div>
+    <div class={ "fixed bg-background-secondary rounded-br-3xl rounded-bl-3xl shadow-lg p-10 max-h-40 top-0 right-0 left-0 z-50 " + props.class }>{props.children}</div>
   )
 }
