3594aa61719fc3fc190317cceafd6a4a63a8b864
Vigneshpeddi <36428047+Vigneshpeddi@users.noreply.github.com>
2023-03-01 23:32:45 -0500

    Add files via upload
    bug fixes (should work, please check)
    subtasks

--- OmnisAlgo/main.py
+++ OmnisAlgo/main.py
@@ -1,3 +1,6 @@
+def condition(element):
+	return element[1], element[2], elemen[3];
+
 def isAvaliableTime(hours1, minutes1, hours2, minutes2, duration1, duration2):
 	start1 = hours1 * 60 + minutes1
 	end1 = start1 + duration1
@@ -13,19 +16,27 @@ def isAvaliableTime(hours1, minutes1, hours2, minutes2, duration1, duration2):
 		return False
 	return True
 
-tasks = [[]]
+ordering = input("Do you like easy or hard tasks first? ")
+pomodoro = input("Would you like to use the pomodoro technique[y/n]? ")
+maxTime = int(input("How long can you work on a task in minutes? "))
+durationTime = int(input("How long do you want to work in a day in minutes? "))
+startHour = int(input("What hour of the day would you like to begin work? "))
+startMinutes = int(input("What minutes of that hour would you like to begin work? "))
+
+tasks = []
 moreTasks = True
 while moreTasks:
 	time = int(input("How long do you expect this task to take you (in minutes)? ")) # include article on how to estimate time on UI, 10 is for breaks
 	priority = int(input("On a scale of 1-10, how importance is this task for you? ")) # include article on how to estimate difficulty on UI
 	deadline = int(input("How many minutes away from January 1st is this task due? "))
 	difficulty = input("What is the difficulty [easy/medium/hard]? ")
-	ordering = input("Do you like easy or hard tasks first? ")
-	pomodoro = input("Would you like to use the pomodoro technique[y/n]? ")
-	maxTime = int(input("How long can you work on a task in minutes? "))
-	durationTime = int(input("How long do you want to work in a day in minutes? "))
-	startHour = int(input("What hour of the day would you like to begin work? "))
-	startMinutes = int(input("What minutes of that hour would you like to begin work? "))
+	subTasks = bool(input("Do you have any subtasks for this task[True/False]? "))
+	while subTasks:
+		stime = int(input("How long do you expect this subtask to take you (in minutes)? ")) # include article on how to estimate time on UI, 10 is for breaks
+		spriority = int(input("On a scale of 1-10, how importance is this subtask for the original task? ")) # include article on how to estimate difficulty on UI
+		sdifficulty = input("What is the difficulty [easy/medium/hard]? ")
+		ssubTasks = bool(input("Do you have any subtasks for this subtask[True/False]? "))
+		tasks.append([stime,sdeadline,spriority,spreference])
 	if ordering == "easy":
 		if difficulty == "easy":
 			preference = 1
@@ -41,26 +52,32 @@ while moreTasks:
 		if difficulty == "hard":
 			preference = 1
 	if time < maxTime:
-		tasks.append(time,deadline,priority, preference)
+		tasks.append([time,deadline,priority, preference])
 	else:
 		while time > maxTime:
-			tasks.append(maxTime, deadline, priority, preference)
+			tasks.append([maxTime, deadline, priority, preference])
 			time -= maxTime
-		tasks.append(time, deadline, priority, preference)
+		tasks.append([time, deadline, priority, preference])
 	moreTasksInput = input("Do you have any more tasks [y/n]? ")
-	moreTasks = True if moreTasksInput = 'y' else 'no'
+	if moreTasksInput == "y":
+		moreTasks = True
+	else:
+		moreTasks = False
 
-tasks = sorted(tasks, key = lambda x : (x[1], x[2], x[3]))
+tasks.sort(key=condition)
 
-obstacles = [[]]
+obstacles = []
 moreObstacles = True
 while moreObstacles:
 	repetition = list(map(str, input("What days of the week will this obstacle be repeated (separated by commas ONLY)? ").split(',')))
 	startTime = input("Type the start time of this task in military time (13:45)? ")
 	duration = int(input("How long in minutes will this osbtacle last (include transportation time)? "))
-	obstacles.append(repetition, int(startTime.split(":")[0]), int(startTime.split(":")[1]), duration)
+	obstacles.append([repetition, int(startTime.split(":")[0]), int(startTime.split(":")[1]), duration])
 	moreObstaclesInput = input("Do you have any more obstacles [y/n]? ")
-	moreObstacles = True if moreObstaclesInput = 'y' else False
+	if moreObstaclesInput == "y":
+		moreObstacles = True
+	else:
+		moreObstacles = False
 
 schedule = [[] * 365]
 daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -73,7 +90,7 @@ for day in range(365):
 		hour = startHour
 		minutes = startMinutes
 		avaliableTime = False
-		while hour < 24 and !avaliableTime:
+		while hour < 24 and not avaliableTime:
 			if pomodoro == "y":
 				task[0] += 40
 			avaliableTime = True
@@ -88,5 +105,10 @@ for day in range(365):
 		if hour >= 24 or dayTime + task[0] > durationTime:
 			continue
 		else:
-			schedule[0].append(hour, minutes, task[0])
+			schedule[0].append([hour, minutes, task[0]])
 			dayTime += task[0]
+
+for task in schedule:
+	for item in task:
+		print(item, end = " ")
+	print("\n")
