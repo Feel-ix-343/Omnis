d7bf5455a0f24def7cd9f8ff236d30146f55e2f5
Felix Zeller <felixazeller@gmail.com>
2023-03-04 10:37:07 -0500

    Docker deployment

--- OmnisAlgo/omnis-algo/src/main.rs
+++ OmnisAlgo/omnis-algo/src/main.rs
@@ -1,15 +1,18 @@
 mod autoschedule_algo;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
 use autoschedule_algo::*;
+use chrono::{Utc, Timelike};
 use serde::{Serialize, Deserialize};
+
+use env_logger;
 
 
 #[derive(Debug, Serialize, Deserialize)]
 struct AutoscheduleRequest {
-    unscheduled_tasks: Vec<Task>,
+    unscheduled_tasks: Vec<UnscheduledTask>,
     user_preferences: UserPreferences,
-    obstacles: Vec<Obstacle>,
+    obstacles: Option<Vec<Obstacle>>,
 }
 
 
@@ -18,19 +21,47 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[get("/autoschedule")]
+#[post("/autoschedule")]
 async fn autoschedule(request: web::Json<AutoscheduleRequest>) -> impl Responder {
-    HttpResponse::Ok().body("Hello autoschedule!")
+    let tasks = request.unscheduled_tasks.clone();
+
+
+    let ordered_tasks = order_tasks(&tasks);
+    let scheduled_tasks = schedule_tasks(&ordered_tasks, Utc::now().with_hour(0).expect("Reset hour to 0").naive_utc());// TODO: Will need to fix this for the user
+
+    let tasks_in_working_hours = adjust_for_working_hours(&scheduled_tasks, &request.user_preferences.start_time, &request.user_preferences.end_time);
+
+    if let Some(obstacles) = &request.obstacles {
+        let tasks_with_obstacles = adjust_for_obstacles(&tasks_in_working_hours, obstacles);
+        return HttpResponse::Ok().json(tasks_with_obstacles);
+    }
+
+    return HttpResponse::Ok().json(tasks_in_working_hours)
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TestJson {
+    name: String
+}
+#[post("/testjson")]
+async fn testjson(request: web::Json<TestJson>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}", &request.name))
+}
+
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(autoschedule)
             .service(hello)
+            .service(testjson)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))? // Binds the app to all interfaces in docker container
     .run()
     .await
 }
