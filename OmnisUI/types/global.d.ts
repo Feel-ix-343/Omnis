c2b3186b8b6e88d5c5b4ed741d77c0e9b988cf34
Felix Zeller <felixazeller@gmail.com>
2023-03-04 11:07:32 -0500

    Update UI types to match new schema

--- OmnisUI/types/global.d.ts
+++ OmnisUI/types/global.d.ts
@@ -1,15 +1,34 @@
-type Task = {
+type UnscheduledTask = {
   id: string,
   name: string,
-  /** Date. Should not have hours, just the date */
-  date: Date,
-  time: number | null,
-  duration: number | null,
-  completed: boolean,
-  priority: number,
   description: string | null,
+
+  /**Duration in minutes */
+  duration: number | null,
+
+
+  importance: "High" | "Low",
+
+  /** Due date of the task. Only the date matters */
+  due_date: Date,
+
+  completed: boolean,
+
   steps: {id: string, duration: number, description: string, completed: boolean, edited: boolean}[] | null
 }
+
+type UnscheduledTaskWithUrgency = {
+  task: UnscheduledTask,
+  urgency: "High" | "Low"
+}
+
+type ScheduledTask = {
+  task: UnscheduledTaskWithUrgency,
+  schedule_datetime: Date
+}
+
+
+// TODO: Algo does not accept tasks without a duration. If duration is null, the task will be considered a daily task. TODO: Make this more clear with types
 
 type DBTask = import ("../src/database/database.types").Database["public"]["Tables"]["tasks"]["Row"]
 
