ea67429020cfe16f56a91688f5ff5c1e754982e4
Felix Zeller <felixazeller@gmail.com>
2023-06-23 20:47:14 -0400

    W fixed + refactorgin

--- omnis-start/src/server/trpc/utils.ts
+++ omnis-start/src/server/trpc/utils.ts
@@ -14,7 +14,44 @@ export const procedure = t.procedure;
 const isAuthed = t.middleware(async (opts) => {
   const {ctx} = opts
 
-  if (ctx.user.aud !== "authenticated") {
+  let cookie = ctx.req.headers.get("cookie")
+  if (!cookie) {
+    throw new TRPCError({code: "UNAUTHORIZED", message: "Missing cookie"})
+  }
+
+  let jwt  = cookie.split("; ").find(s => s.indexOf("my-access-token") !== -1)?.split("=")[1]
+  let refresh  = cookie.split("; ").find(s => s.indexOf("my-refresh-token") !== -1)?.split("=")[1]
+  //console.log("jwt", jwt)
+  //console.log("refresh", refresh)
+
+  if (!jwt) {
+    throw new TRPCError({code: "UNAUTHORIZED", message: "Missing JWT"})
+  }
+
+  if (!refresh) {
+    throw new TRPCError({code: "UNAUTHORIZED", message: "Missing refresh token"})
+  }
+
+
+  const supabaseURL = process.env.SERVER_SUPABASE_URL!
+  const supabaseKey = process.env.SERVER_SUPABASE_KEY!
+
+  // test fetch
+  // headers: 
+  // apiKey: SUPABSE_KEY
+  // Authorization: Bearer JWT
+  // remove connection header
+  const res = await fetch(supabaseURL + "/auth/v1/user", {
+    headers: {
+      "apiKey": supabaseKey,
+      "Authorization": "Bearer " + jwt,
+    }
+  })
+
+  const user: User = await res.json() as User
+  console.log(user)
+
+  if (user.aud !== "authenticated") {
     throw new TRPCError({code: "UNAUTHORIZED", message: "Auth error"})
   }
 
