310905421522ded52f31627fa45c137f36b83acd
Felix Zeller <felixazeller@gmail.com>
2023-06-25 00:15:47 -0400

    Remove old UI

--- OmnisUI/src/utils/database/supabaseClient.ts
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "./database.types";
-
-export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL!, import.meta.env.VITE_SUPABASE_KEY!);
