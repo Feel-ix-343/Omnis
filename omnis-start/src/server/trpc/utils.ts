e5a393568cac78b8800cd63a55e7f1683a745059
Felix Zeller <felixazeller@gmail.com>
2023-06-25 00:13:38 -0400

    Remove random imports

--- omnis-start/src/server/trpc/utils.ts
+++ omnis-start/src/server/trpc/utils.ts
@@ -1,8 +1,4 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import {fetchRequestHandler} from '@trpc/server/adapters/fetch'
-import { createClient, User } from "@supabase/supabase-js";
-import { initTRPC, TRPCError } from "@trpc/server";
-import { Database } from "~/utils/database/database.types";
+import { initTRPC } from "@trpc/server";
 import type { IContext } from "./context";
 
 export const t = initTRPC.context<IContext>().create();
