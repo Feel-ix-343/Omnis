623b0ee0b580fb85962dd90aed82950b438a10d4
Felix Zeller <felixazeller@gmail.com>
2023-09-14 09:22:02 -0400

    Fix rerendering error in new task creation

--- omnis-next/components/todos.tsx
+++ omnis-next/components/todos.tsx
@@ -18,17 +18,9 @@ export default function(props: {todos: Todo[] | null, user: User}) {
     (todos: Todo[] | null, action) => !action.delete ? [...todos ?? [], action.todo] : todos?.filter(t => t!.id !== action.todo!.id) ?? []
   )
 
-
-  const [creating, setCreating] = useState(false)
-  const [newTaskTitle, setNewTaskTitle] = useState<string | null>(null)
-
-
   const supabase = createClientComponentClient<Database>()
 
   const createTodo = async (title: string) => {
-    setCreating(false)
-    setNewTaskTitle("")
-
     const todo: Todo = {title, is_complete: false, user_id: props.user.id, created_at: (new Date()).toUTCString(), id: crypto.randomUUID()}
     changeOptimistic({todo})
 
@@ -59,24 +51,32 @@ export default function(props: {todos: Todo[] | null, user: User}) {
   return <>
     <div className="flex flex-col gap-4 w-4/12">
       <h3 className="h-10">Todos</h3>
-      {!creating ? 
-        <Card draggable className="bg-secondary transition-shadow hover:cursor-pointer" onClick={() => setCreating(true)} key="new">
-          <CardHeader><CardTitle className="font-sans text-sm">New Todo</CardTitle></CardHeader>
-        </Card>
-        :
-        <Card draggable className="bg-secondary transition-shadow hover:cursor-pointer shadow-lg" key="new">
-          <CardHeader><CardTitle className="font-sans text-sm"><Input autoFocus placeholder="Todo Name" value={newTaskTitle ?? undefined} onChange={e => setNewTaskTitle(e.target.value)} className="bg-secondary focus:bg-white" /></CardTitle></CardHeader>
-
-          <CardFooter>
-            <Button variant="outline" className="ml-auto" onClick={() => newTaskTitle && createTodo(newTaskTitle)}>Save</Button>
-          </CardFooter>
-        </Card>
-      }
+      <CreateTask createTask={createTodo} />
       {optimisticMessages?.map(t => t && 
         <Card draggable onClick={() => deleteTodo(t)} className="hover:shadow-lg hover:scale-[102%] transition-all hover:cursor-pointer" key={t.id}>
           <CardHeader><CardTitle className="font-sans text-sm">{t.title}</CardTitle></CardHeader>
         </Card>
       )}
     </div>
+  </>
+}
+
+function CreateTask(props: {createTask: (t: string) => void}) {
+  const [creating, setCreating] = useState(false)
+  const [newTaskTitle, setNewTaskTitle] = useState<string | null>(null)
+  return <>
+    {!creating ? 
+      <Card draggable className="bg-secondary transition-shadow hover:cursor-pointer" onClick={() => setCreating(true)} key="new">
+        <CardHeader><CardTitle className="font-sans text-sm">New Todo</CardTitle></CardHeader>
+      </Card>
+      :
+      <Card draggable className="bg-secondary transition-shadow hover:cursor-pointer shadow-lg" key="new">
+        <CardHeader><CardTitle className="font-sans text-sm"><Input autoFocus placeholder="Todo Name" value={newTaskTitle ?? undefined} onChange={e => setNewTaskTitle(e.target.value)} className="bg-secondary focus:bg-white" /></CardTitle></CardHeader>
+
+        <CardFooter>
+          <Button variant="outline" className="ml-auto" onClick={() =>{setNewTaskTitle(""); setCreating(false); newTaskTitle && props.createTask(newTaskTitle)}}>Save</Button>
+        </CardFooter>
+      </Card>
+    }
   </>
 }
