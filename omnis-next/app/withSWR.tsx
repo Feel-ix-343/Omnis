c567d2f380066dbdd04c215f8c25eacda6ad2a25
Felix Zeller <felixazeller@gmail.com>
2023-09-16 00:45:58 -0400

    Initial SSR, then client side.

--- omnis-next/app/withSWR.tsx
+++ omnis-next/app/withSWR.tsx
@@ -4,7 +4,7 @@ import { SWRConfig } from "swr";
 
 export default function WithSWR(props: {children: React.ReactNode, fallback: any}) {
   console.log(props.fallback)
-  return <SWRConfig >
+  return <SWRConfig value={{fallback: props.fallback}}>
     {props.children}
   </SWRConfig>
 }
